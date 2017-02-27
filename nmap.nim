06030454ce9aed736db8ed71319aa677fe14b54c
Jacob Doyelle Yates <blmvxer@gmail.com>
2017-02-27 16:06:39 -0500

    Update nmap.nim

--- nmap.nim
+++ nmap.nim
@@ -15,10 +15,17 @@ import strutils, httpclient
 type host = string
 
 proc nmap_scan*(host, port: string): Response {.exportc.} =
-   var
-      timeout = 5
-      client = newHttpClient(timeout = timeout * 1000)
-      url = "http://" & host & ":" & port
-      resp = client.request(url)
-   echo resp.status
-
+   try:
+      var
+         timeout = 5
+         client = newHttpClient(timeout = timeout * 1000)
+         url = "http://" & host & ":" & port
+         resp = client.request(url)
+      echo resp.status
+   except:
+      let testPort: string = getCurrentExceptionMsg()
+      let portResp = find(testPort, "SSH")
+      if portResp != -1:
+         echo split(testPort, "invalid http version, ")
+      else:
+         echo "SSH port not open or filtered"
