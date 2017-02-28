fd23f284d89fe2472ef9ceedffee5bd7c17ad434
Jacob Doyelle Yates <blmvxer@gmail.com>
2017-02-28 00:10:34 -0500

    Update nmap.nim

--- nmap.nim
+++ nmap.nim
@@ -10,27 +10,42 @@
 
 {.deadCodeElim: on.}
 
-import strutils, httpclient
+import strutils, httpclient, os, sequtils
 
 type
    host = string
+   port = string
    lHost = bool
    lPort = bool
    rHost = bool
    rPort = bool
+   portDes = enum
+      ftp, sftp, ssh, telnet, http, https
 
 var
-   portNum = [20, 21, 22, 23, 80, 443]##Common ports
-   portDes = ["ftp", "sftp", "ssh", "telnet", "http", "https"]
+   portArg = ("common")
+   portNum: array[6, string]
+portNum = ["20", "21", "22", "23", "80", "443"]#Common ports
+
+################################################################
+
+proc nmap_iface*(): int {.exportc.} = ##Display current network interfaces
+   let iFace = execShellCmd("ifconfig")
+   return (iFace)
 
 proc nmap_scan*(host, port: string): Response {.exportc.} = ##basic nmap and port scan
    try:
-      var
-         timeout = 5
-         client = newHttpClient(timeout = timeout * 1000)
-         url = "http://" & host & ":" & port
-         resp = client.request(url)
-      echo resp.status
+      var timeout = 5
+      var client = newHttpClient(timeout = timeout * 1000)
+      if port == portArg:
+         for i in low[portNum]..high[portNum]:
+            var url = "http://" & host & ":" & portNum[i]
+            var resp = client.request(url)
+            echo resp.status
+      else:
+         var url = "http://" & host & ":" & port
+         var resp = client.request(url)
+         echo resp.status
    except:
       let testPort: string = getCurrentExceptionMsg()
       let portResp = find(testPort, "SSH")
