071bdb86335a317bb6e9b4814d6e6d021c013581
Jacob Doyelle Yates <blmvxer@gmail.com>
2017-03-09 14:00:17 -0500

    Update nmap_os_scan.nim

--- nmap_os_scan.nim
+++ nmap_os_scan.nim
@@ -1,31 +1,27 @@
-import nativesockets
-from posix import fork
+import net, nmap
 
-const sockBuff = 65596
+const 
+   TCP = IPPROTO_TCP
+   UDP = IPPROTO_UDP
+   RAW = IPPROTO_RAW
+   ICMP = IPPROTO_ICMP
+   IPv4 = AF_INET
+   IPv6 = AF_INET6
+   STREAM = SOCK_STREAM
+   DGRAM = SOCK_DGRAM
+   sRAW = SOCK_RAW
+   SEQPACKET = SOCK_SEQPACKET
+   SSH = 22
+   TELNET = 23
+   HTTP = 80
+   HTTPS = 443
 
-var
-   data = newString(sockBuff)
-   pData: pointer = data.addr
-   addrHost = cast[ptr uint32](cast[int]("192.168.1.110".cstring))[]
-   port = 80
-   sock = newNativeSocket(AF_INET, SOCK_RAW, IPPROTO_ICMP)
-sock.setSockOptInt(cint(SOL_SOCKET), SO_REUSEADDR, 1)
-sock.setBlocking(false)
-discard fork()
+proc osscan(host: string, port: int) = 
+   try:
+      var sock = newSocket(IPV4, STREAM, TCP)
+      sock.connect(host, Port(port))
+   except:
+      let ErrorMsg = getCurrentExceptionMsg()
+      echo ErrorMsg
 
-var name: SockAddr_in
-name.sin_family = toInt(AF_INET)
-name.sin_port = htons(80'u16)
-name.sin_addr.s_addr = addrHost
-
-discard sock.bindAddr(cast[ptr SockAddr](addr name), Socklen(sizeof(name)))
-discard sock.listen()
-
-var
-   sockAddress: Sockaddr_in
-   addrLen = Socklen(sizeof(sockAddress))
-   osDet = sock.bindAddr(cast[ptr SockAddr](addr(sockAddress)), addrLen)
-
-let response = sock.recvfrom(pData, sockBuff, O_NONBLOCK, cast[ptr SockAddr](addr(sockAddress)), addr(addrLen))
-echo response
-sock.close()
+osscan("192.168.0.11", 24)
