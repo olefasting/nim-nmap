22da128c25dfac611e664df9e718abe9a63645b3
Jacob Doyelle Yates <blmvxer@gmail.com>
2017-03-14 10:51:00 -0400

    Update templates.nim

--- templates.nim
+++ templates.nim
@@ -1,4 +1,12 @@
-##xSpecPort data retrieval and test
+import nmap, strutils
+#var host = "192.168.0.11"
+#for i in countup(1, 999):
+#   portList.add(i)
+#createMask("192.168.1.1/24")
+#for i in netMask:
+#   nmapScan(i, 80)
+
+
 let myBool = false
 
 var x = 0
@@ -7,3 +15,8 @@ for i in winSpecPort:
    echo myBool
    echo winSpecPort[x]
    inc(x)
+
+nmapHostDisc()
+for v in hostDisc:
+   for p in winSpecPort:
+      nmapScan(v, p)
